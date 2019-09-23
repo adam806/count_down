2d66a317028c185132e55c7c56c508e053e1d099
adamhan <806514395@qq.com>
2019-09-23 19:15:59 +0800

    修改自动多次调用bug

--- count_dowm.js
+++ count_dowm.js
@@ -1,15 +1,13 @@
 let figlet = require('figlet');
 const end_time = 1598889600000; //20200901
 let current_time = new Date().getTime();
-let refresh_time = 300000;//5分钟刷新一次
-let current_day = new Date(current_time).getDate();
+let refresh_time = 1569149222000;//5分钟刷新一次
+let refresh_day = 0;
 
 get_remain_time(end_time, current_time, false);
+
 let timer = setInterval(() => {
     current_time = new Date().getTime();
-    current_day = new Date(current_time).getDate();
-    get_remain_time(end_time, current_time, true);
-
     if (current_time >= end_time) {
         figlet(`Time up`, function (err, data) {
             if (err) {
@@ -19,15 +17,19 @@ let timer = setInterval(() => {
             }
             console.log(data)
         });
+        clearInterval(timer);
     }
+    get_remain_time(end_time, current_time, true);
+
 }, refresh_time)
 
-function get_remain_time(endTime, currentTime, refresh) {
-    let remain_time = endTime - currentTime;//timestamp
-    let refresh_day = new Date(currentTime).getDate();
-    let day = Math.floor(remain_time / 1000 / 60 / 60 / 24);
+function get_remain_time(end_time, current_time, refresh) {
+    let remain_time = end_time - current_time;//timestamp差值
+    let display_day = Math.floor(remain_time / 1000 / 60 / 60 / 24);//显示day
+    let current_day = new Date(current_time).getDate();//当前调取本方法的时间
     if (!refresh) {
-        figlet(`${day}!`, function (err, data) {
+        refresh_day = new Date(current_time).getDate()
+        figlet(`${display_day}!`, function (err, data) {
             if (err) {
                 console.log('Something went wrong...');
                 console.dir(err);
@@ -35,9 +37,10 @@ function get_remain_time(endTime, currentTime, refresh) {
             }
             console.log(data)
         });
-    } else if (refresh && refresh_day - current_day >= 1) {
+    } else if (refresh && (current_day - refresh_day >= 1)) {
+        refresh_day = new Date(current_time).getDate()
         console.clear()
-        figlet(`${day}!`, function (err, data) {
+        figlet(`${display_day}!`, function (err, data) {
             if (err) {
                 console.log('Something went wrong...');
                 console.dir(err);
